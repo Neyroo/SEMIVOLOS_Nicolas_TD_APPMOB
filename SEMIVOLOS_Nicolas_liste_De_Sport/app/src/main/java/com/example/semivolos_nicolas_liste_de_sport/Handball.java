6647c83b013e3b26b04d60859fff13c08095e197
NicolasSemivolos <nicolas67000@free.fr>
2021-01-22 18:23:16 +0100

    TD2_Liste_de_sport
    Appli sur les listes de sport avec navigation

---
+++ SEMIVOLOS_Nicolas_liste_De_Sport/app/src/main/java/com/example/semivolos_nicolas_liste_de_sport/Handball.java
@@ -0,0 +1,27 @@
+package com.example.semivolos_nicolas_liste_de_sport;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Handball extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_handball);
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Button btn_prev = findViewById(R.id.prev);
+
+        btn_prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
